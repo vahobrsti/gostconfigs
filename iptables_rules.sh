de481950495a3b07e6b23702ba0d55cd4353a893
Vahab Rasti <vahab.rasti@gmail.com>
2023-06-02 11:58:10 +1000

    feat(iptables): exclude panel and ntop port from the list and block them

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -3,8 +3,8 @@
 # Print the iptables rule to allow established and related TCP connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Get a comma-separated list of all TCP ports that are currently open and store them in a variable
-open_ports=$(netstat -ntlp | awk 'NR > 2 && $6 == "LISTEN" { split($4, a, ":"); printf "%s\n", a[length(a)] }' | sort -u |  tr '\n' ','| sed 's/,$//')
+# Get a comma-separated list of all TCP ports that are currently open and store them in a variable. Exclue 8001 and 3000. 8001 is the panel port, 3000 is the ntop port.
+open_ports=$(netstat -ntlp | awk 'NR > 2 && $6 == "LISTEN" { split($4, a, ":"); printf "%s\n", a[length(a)] }' | grep -vE '8001|3000' | sort -u | tr '\n' ',' | sed 's/,$//')
 # Print a single iptables rule that allows incoming traffic on all the open TCP ports
 iptables -A INPUT -p tcp -m multiport --dports $open_ports -j ACCEPT
 
