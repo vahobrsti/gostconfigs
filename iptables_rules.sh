7224538fae5871332ba0b460a0f7a033a6b31ff1
Vahab Rasti <vahab.rasti@gmail.com>
2023-09-16 10:35:52 +1000

    feat(iptables): updated to add localhost + docker + internal traffic

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
 iptables -F
 # Print the iptables rule to allow established and related TCP connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Get a comma-separated list of all TCP ports that are currently open and store them in a variable. Exclue 8001 and 3000. 8001 is the panel port, 3000 is the ntop port.
-open_ports=$(netstat -ntlp | awk 'NR > 2 && $6 == "LISTEN" { split($4, a, ":"); printf "%s\n", a[length(a)] }' | grep -vE '8001|3000|62789|6379' | sort -u | tr '\n' ',' | sed 's/,$//')
-# Print a single iptables rule that allows incoming traffic on all the open TCP ports
+# Get a comma-separated list of all TCP ports that are currently open and store them in a variable.
+open_ports=$(netstat -ntlp | awk 'NR > 2 && $6 == "LISTEN" && $4 !~ /127\.0\.0\.1/ { split($4, a, ":"); printf "%s\n", a[length(a)] }' | sort -u | tr '\n' ',' | sed 's/,$//')
+# Get a comma-separated list of all CIDR ranges for the interfaces with a global scope
+cidr_ranges=$(ip -o -f inet addr show | awk '/scope global/ {split($4, a, " "); printf "%s\n", a[1]}' | tr '\n' ',' | sed 's/,$//')
 iptables -A INPUT -p tcp -m multiport --dports $open_ports -j ACCEPT
-
-# Print a final rule that drops any other incoming TCP connections
+iptables -A INPUT -s $cidr_ranges -j ACCEPT
+# add localhost
+iptables -A INPUT -s 127.0.0.1/8 -j ACCEPT
+# a final rule that drops any other incoming TCP connections
 iptables -A INPUT -p tcp -j DROP
