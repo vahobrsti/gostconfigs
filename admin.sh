a4fa816b01a53ed9cc0616f5c3fd3c60c1ec0596
vahob <vahab.rasti@gmail.com>
2024-02-23 05:19:37 +0000

    feat(admin.sh): wait before running netstat

--- admin.sh
+++ admin.sh
@@ -234,6 +234,7 @@ server_installation() {
     cp ocserv.conf /etc/ocserv/
     cp ocpasswd /etc/ocserv/
     # enable ip forwarding
+    iptables -P FORWARD ACCEPT
     echo "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/60-custom.conf
     echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.d/60-custom.conf
     echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.d/60-custom.conf
